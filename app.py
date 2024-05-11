1fa2b31d33eaaaeb9791c49535fdd2670d4e2141
Phobos-Duck <161325120+Phobos-Duck@users.noreply.github.com>
2024-05-12 02:43:04 +0600

    стили в отдельном файле + таблица значений

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, request
-from math import sin, tan, cos, pi
+from math import sin, tan, cos, pi, asin, acos, atan
 
 
 app = Flask(__name__)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def calculate():
@@ -11,15 +10,21 @@ def calculate():
         angle = float(request.form['angle'])
         precision = int(request.form['precision'])
         units = request.form['units']
-        result = ""
+        result = {}
 
         if units == 'degrees':
             angle = angle * pi / 180
 
-        result += f"<p>Синус: {round(sin(angle), precision)}</p>"
-        result += f"<p>Косинус: {round(cos(angle), precision)}</p>"
-        result += f"<p>Тангенс: {round(tan(angle), precision)}</p>"
-        result += f"<p>Котангенс: {round(1 / tan(angle), precision)}</p>"
+        result = {
+            'sin': round(sin(angle), precision),
+            'cos': round(cos(angle), precision),
+            'tg': round(tan(angle), precision),
+            'ctg': round(1 / tan(angle), precision),
+            'asin': round(asin(angle), precision),
+            'acos': round(acos(angle), precision),
+            'atg': round(atan(angle), precision),
+            'actg': round(1/atan(angle), precision)
+        }
 
         return render_template("trig_calulator.html", result=result)
 
